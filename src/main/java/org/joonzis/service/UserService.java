2811cfc7ce82f2852062a0dea2c4ddd8b6eccc4f
DongWooJang8238 <ehddnwn@skkukdp.re.kr>
2024-10-18 10:07:20 +0900

    2024-10-18 10:07

--- src/main/java/org/joonzis/service/UserService.java
+++ src/main/java/org/joonzis/service/UserService.java
@@ -37,6 +37,9 @@ public interface UserService {
 	// 유저 정보 업데이트
 	public int updateUserInfo(UserVO vo);
 	
+	// 유저 정보 조회 ( by.mno )
+	public UserVO userSelectOne(int mno);
+	
 	// 유저 탈퇴
 	public int deleteAccount(String userId);
 }
